d66930c90759d54a1708bfa72fc961eb90ffccfa
alfinur154 <alfinur27@gmail.com>
2023-06-15 14:55:02 +0700

    Add project files.

---
+++ stack_104/stack_104.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class StackArray {
+private:
+    string stack_array[5];
+    int top;
+public:
+
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
